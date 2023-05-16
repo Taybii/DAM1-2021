8b803d5b999d789d5540d8f265bd8ed7ae863157
Taybii <ataybi.dam@institutcampalans.net>
2023-05-16 15:43:37 +0200

    Calculadora

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriu un numero");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escriu un altre numero");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
